2a4d137d42086fd3eca2369db9b1a2eed9c2a3bf
MihailPetkov96 <mpetkov228@gmail.com>
2024-08-20 15:52:38 +0300

    Add express static middleware

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('body', request => {
 const app = express();
 
 app.use(express.json());
+app.use(express.static('dist'));
 app.use(cors());
 app.use(
     morgan(':method :url :status :res[content-length] - :response-time ms :body')
