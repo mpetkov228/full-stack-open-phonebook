08a73aa6a3c6e154c239f71a84cf662a269e968f
MihailPetkov96 <mpetkov228@gmail.com>
2024-08-24 13:23:52 +0300

    Configure eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,13 @@
 import globals from 'globals';
+import js from '@eslint/js';
+import stylisticJs from '@stylistic/eslint-plugin-js';
 
 
 export default [
+  js.configs.recommended,
+  {
+    ignores: ['dist/**']
+  },
   {
     files: ['**/*.js'], 
     languageOptions: {
@@ -10,6 +16,32 @@ export default [
         ...globals.node
       },
       ecmaVersion: 'latest'
-    }
+    },
+    plugins: {
+      '@stylistic/js': stylisticJs
+    },
+    rules: {
+      '@stylistic/js/indent': [
+        'error',
+        2
+      ],
+      '@stylistic/js/linebreak-style': [
+        'error',
+        'windows'
+      ],
+      '@stylistic/js/quotes': [
+        'error',
+        'single'
+      ],
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+        'error', 'always'
+      ],
+      'arrow-spacing': [
+        'error', { 'before': true, 'after': true }
+      ],
+      'no-console': 'off',
+    },
   },
 ];
